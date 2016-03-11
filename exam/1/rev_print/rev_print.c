fac86f1dc1485184fffd51690d4d6c2e08f0852e
Jules BARBIER <jubarbie@e1r7p9.42.fr>
2016-03-11 19:17:28 +0100

    rev_print optimization

--- exam/1/rev_print/rev_print.c
+++ exam/1/rev_print/rev_print.c
@@ -6,32 +6,24 @@
 /*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/08 10:05:58 by jubarbie          #+#    #+#             */
-/*   Updated: 2016/03/08 10:10:02 by jubarbie         ###   ########.fr       */
+/*   Updated: 2016/03/11 19:16:38 by jubarbie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_strlen(char *str)
+void	rev_print(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+	if (*str)
+		rev_print(str + 1);
+	if (*str)
+		write(1, str, 1);
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	int	i;
-
 	if (ac == 2)
-	{
-		i = ft_strlen(av[1]);
-		while (--i >= 0)
-			write(1, &(av[1][i]), 1);
-	}
+		rev_print(av[1]);
 	write(1, "\n", 1);
 	return (0);
 }
