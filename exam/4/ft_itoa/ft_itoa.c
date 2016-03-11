b96ccbb5f752ce2ba708c82410891e2441373490
Jules BARBIER <jubarbie@e1r7p9.42.fr>
2016-03-11 19:42:43 +0100

    ft_itoa optimization

--- exam/4/ft_itoa/ft_itoa.c
+++ exam/4/ft_itoa/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/08 15:21:57 by jubarbie          #+#    #+#             */
-/*   Updated: 2016/03/08 16:43:40 by jubarbie         ###   ########.fr       */
+/*   Updated: 2016/03/11 19:42:03 by jubarbie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,8 @@ int	ft_nblen(int nb)
 	int i;
 
 	i = (nb >= 0) ? 1 : 2;
-	while (nb / 10)
-	{
-		nb /= 10;
+	while (nb /= 10)
 		i++;
-	}
 	return (i);
 }
 
@@ -37,21 +34,15 @@ char *ft_itoa(int nbr)
 	int nblen;
 	char *str;
 
+	if (nbr == -2147483648)
+		return ("-2147483648");
 	nblen = ft_nblen(nbr);
-	str = (char *)malloc(sizeof(char) * nblen + 1);
-	if (str == NULL)
+	if (!(str = (char *)malloc(sizeof(char) * nblen + 1)))
 		return (NULL);
 	if (nbr < 0)
 		str[0] = '-';
-	if (nbr == -2147483648)
-	{
-		ft_doatoi(214748364, &str[nblen - 2]);
-		str[nblen - 1] = '8';
-		str[nblen] = '\0';
-		return (str);
-	}
 	nbr *= (nbr < 0) ? -1 : 1;
-	str[nblen--] = '\0';
-	ft_doatoi(nbr, &str[nblen]);
+	str[nblen] = '\0';
+	ft_doatoi(nbr, &str[--nblen]);
 	return (str);
 }
