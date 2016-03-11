afc2dac4accf8b18aefb5ee2fe3165cd0f3718c6
Jules BARBIER <jubarbie@e1r7p9.42.fr>
2016-03-11 19:48:01 +0100

    alignment of functions' declarations

--- exam/4/rev_wstr/rev_wstr.c
+++ exam/4/rev_wstr/rev_wstr.c
@@ -6,7 +6,7 @@
 /*   By: jubarbie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/08 10:54:13 by jubarbie          #+#    #+#             */
-/*   Updated: 2016/03/08 11:13:44 by jubarbie         ###   ########.fr       */
+/*   Updated: 2016/03/11 19:47:46 by jubarbie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	rev_wstr(char *str, int i)
 {
-	char *ptr;
+	char	*ptr;
 
 	ptr = str;
 	while (*ptr && *ptr != ' ')
@@ -27,7 +27,7 @@ void	rev_wstr(char *str, int i)
 		write(1, " ", 1);
 }
 
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	if (ac == 2)
 		rev_wstr(av[1], 0);
